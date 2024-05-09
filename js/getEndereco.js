d37ba71c6d717f70d5454447b2212d6fb4b5d592
Marcus <107251339+marcusdev29@users.noreply.github.com>
2024-05-09 18:21:27 -0300

    exibir na tela de endereco

--- js/getEndereco.js
+++ js/getEndereco.js
@@ -1,11 +1,30 @@
 const url = 'https://go-wash-api.onrender.com/api/auth/address';
 
-async function getEnderecoUsuario(){   
+/*function adicionarEndereco(endereco) {
+    listaEnderecos.push(endereco);
+}
+
+// Função para exibir todos os endereços na página
+function exibirEnderecos() {
+    const listaDiv = document.getElementById('dados-api');
+
+    // Itera sobre a lista de endereços e exibe cada endereço
+    listaEnderecos.forEach(function(endereco) {
+        const enderecoDiv = document.createElement('div');
+        enderecoDiv.textContent = endereco;
+        listaDiv.appendChild(enderecoDiv);
+    });
+}    
+*/
+
+
+
+document.addEventListener("DOMContentLoaded", async function getEnderecoUsuario(){   
     let user  = localStorage.getItem('user');
     let token  =  JSON.parse(user).access_token;
     console.log(JSON.parse(localStorage.getItem('user')).access_token);
     
-    let resposta = await fetch(url,{
+    const resposta = await fetch(url,{
         method: "GET",
         headers:{
             'Content-Type': 'application/json',
@@ -13,20 +32,34 @@ async function getEnderecoUsuario(){
         }      
     });
     
-    let data = await resposta.json();
+    const data = await resposta.json();
+    exibirDados(data);
     console.log(data);
-    /*
-    if (resposta.ok) {
-        alert("Endereço realizado com sucesso!"); // Exibe um alerta se o cadastro for bem-sucedido
-         window.location.href = "endereco.html"; // Redireciona para a página de endereco
-    } else {
-        if (data && data.errors) { //condição que verifica se o objeto data está definido e se possui uma propriedade errors.
-            // Se houver erros retornados pela API, exibe esses erros
-            alert("Erro ao enviar o endereço: " + JSON.stringify(data.errors)); //JSON.stringify(data.errors) esta convertendo o objeto de erro em uma string Json
-        } else {
-            alert("Ocorreu um erro ao enviar o endereço. Por favor, tente novamente."); // Exibe um alerta genérico de erro
-        }
-    }
-    */
+
+});
+
+function exibirDados(data) {
+    const dadosDiv = document.getElementById('dados-api');
+
+    dadosDiv.innerHTML = `<pre>${JSON.stringify(data)}</pre>`;
+};
+
+function exibirDadosTabela(data){
+    const tabela = document.getElementById('tabela');
+    const tbody = tabela.getElementsByTagName('tbody')[0];
+
+     // Itera sobre a lista de endereços e exibe cada endereço
+    data.forEach(function(endereco) {
+        const novaLinha = document.createElement('tr');
+        novaLinha.innerHTML =  `
+        <td>${endereco.title}</td>
+        <td>${endereco.cep}</td>
+        <td>${endereco.address}</td>
+        <td>${endereco.number}</td>
+        <td>${endereco.complement}</td>
+    `;
+        tbody.appendChild(novaLinha);
+    });
 }
+
 getEnderecoUsuario();
