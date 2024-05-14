3ca763f72029a1a4d5a47f712673bffa960d85a2
Marcus <107251339+marcusdev29@users.noreply.github.com>
2024-05-13 22:31:43 -0300

    comentarios

--- js/getEndereco.js
+++ js/getEndereco.js
@@ -2,12 +2,8 @@ const url = 'https://go-wash-api.onrender.com/api/auth/address';
 
 async function getEnderecoUsuario() {
     try {
-        let user = localStorage.getItem('user');
-        if (!user) {
-            console.log("Usuário não encontrado.");
-            return;
-        }
-        let token = JSON.parse(user).access_token;
+        let user = localStorage.getItem('user'); //obtém os dados do usuário armazenados no local storage do navegador.
+        let token = JSON.parse(user).access_token; // analisa o JSON para extrair o token de acesso.
 
         const resposta = await fetch(url, {
             method: "GET",
@@ -20,8 +16,8 @@ async function getEnderecoUsuario() {
         const data = await resposta.json();
         console.log(data);
 
-        if (data && data.data && data.data.length > 0) {
-            exibirDados(data.data);
+        if (data && data.data && data.data.length > 0) { //verifica se os dados existem e se contêm pelo menos um endereço.
+            exibirDados(data.data); //para exibir os endereços na tabela.
         } else {
             console.log("Nenhum endereço encontrado.");
         }
@@ -30,7 +26,7 @@ async function getEnderecoUsuario() {
     }
 }
 
-function exibirDados(enderecos) {
+function exibirDados(enderecos) { //recebe uma lista de endereços e os exibe na tabela.
     const tabela = document.getElementById('dados-api');
 
     enderecos.forEach(endereco => {
@@ -40,7 +36,10 @@ function exibirDados(enderecos) {
 }
 
 function criaLinha(endereco) {
-    const linha = document.createElement("tr");
+    const linha = document.createElement("tr"); //cria uma linha de tabela (<tr>) para um endereço.
+
+    /*Para cada campo do endereço, cria uma célula de tabela (<td>), define seu conteúdo (textContent), 
+    e adiciona a célula à linha. */
 
     const tdId = document.createElement("td");
     const tdTitle = document.createElement("td");
@@ -56,6 +55,9 @@ function criaLinha(endereco) {
     tdNumber.textContent = endereco.number;
     tdComplement.textContent = endereco.complement || "-"; // Se complemento for null, exibe "-"
 
+    /*O método appendChild é usado para construir dinamicamente a estrutura da linha da tabela,
+     adicionando cada célula na ordem correta.*/
+     
     linha.appendChild(tdId);
     linha.appendChild(tdTitle);
     linha.appendChild(tdCEP);
